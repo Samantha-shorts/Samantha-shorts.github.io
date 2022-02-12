65d3d1713aa338e2b3d594a60a6e360e0e552244
KamiteAkira0611 <KamiteAkira0611@users.noreply.github.com>
2022-02-12 15:25:13 +0000

    deploy: dcc8c52e60eae928731cadc58bacf242addb77cc

---
+++ _next/static/WGOxqQWBGN-cnizu6buft/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/821dae8904e1a2cf.css","static/chunks/pages/index-60d95f549dad22dc.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
