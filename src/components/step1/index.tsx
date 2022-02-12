7f0ec613bae0517857d4d2efe7c64b9439990bd9
KamiteAkira0611 <kamiteakira@gmail.com>
2022-02-12 22:01:41 +0900

    save

--- src/components/step1/index.tsx
+++ src/components/step1/index.tsx
@@ -4,18 +4,34 @@ import styles from "./styles.module.scss"
 const Step1: NextPage = () => {
   return (
     <div className={styles.step1}>
-      <img src="/images/logo.png" />
-      <h1>世界のショート映画をSAMANSAで</h1>
-      <p>
+      <img className={styles.logo} src="/images/logo.png" />
+      <h1 className={styles.title}>世界のショート映画を<span>SAMANSA</span>で</h1>
+      <p className={styles.txt}>
         SAMANSAは世界中の良質で面白いショート映画・ドラマ・ドキュメンタリーを発掘し、<br/>
         クリエイターと直接契約を交わして配信しているプラットフォームです。<br/>
       </p>
-      <h2>料金プラン</h2>
-      <p>
+      <h2 className={styles.subtitle}>料金プラン</h2>
+      <p className={styles.txt}>
         クレジットカードの登録が不要でご利用いただけます。<br/>
         2週間トライアル付き(以降月額250円)。<br/>
         ※トライアル終了後は自動課金ではありません。
       </p>
+
+      <div className={styles.boxes}>
+        <div className={styles.box}>
+          <p>
+            月額<br/>
+            <span className={styles.big}>250</span>円
+          </p>
+        </div>
+
+        <div className={styles.box}>
+          <p>
+            14日間<br/>
+            <span>無料</span>体験
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,14 @@
 import type { NextPage } from 'next'
+import Head from 'next/head'
 import Step1 from '../components/step1'
 import styles from '../styles/Home.module.scss'
 
 const Home: NextPage = () => {
   return (
     <div className={styles.root}>
+      <Head>
+        <title>SAMANSA</title>
+      </Head>
       <Step1 />
     </div>
   )
