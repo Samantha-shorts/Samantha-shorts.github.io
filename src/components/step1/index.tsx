60d7c053690f1e4d0ddc138be71d5103630625fb
KamiteAkira0611 <kamiteakira@gmail.com>
2022-02-12 20:14:03 +0900

    first commit

---
+++ src/components/step1/index.tsx
@@ -0,0 +1,24 @@
+import { NextPage } from "next"
+import Image from "next/image"
+import styles from "./styles.module.scss"
+
+const Step1: NextPage = () => {
+  return (
+    <div className={styles.step1}>
+      <Image width={72} height={16} src="/images/logo.png" />
+      <h1>世界のショート映画をSAMANSAで</h1>
+      <p>
+        SAMANSAは世界中の良質で面白いショート映画・ドラマ・ドキュメンタリーを発掘し、<br/>
+        クリエイターと直接契約を交わして配信しているプラットフォームです。<br/>
+      </p>
+      <h2>料金プラン</h2>
+      <p>
+        クレジットカードの登録が不要でご利用いただけます。<br/>
+        2週間トライアル付き(以降月額250円)。<br/>
+        ※トライアル終了後は自動課金ではありません。
+      </p>
+    </div>
+  )
+}
+
+export default Step1
---
+++ src/components/step2/index.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from 'next'
+
+const Step2:NextPage = () => {
+  return (
+    <div>Step2</div>
+  )
+}
+
+export default Step2
