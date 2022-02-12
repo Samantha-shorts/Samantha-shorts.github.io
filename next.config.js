91b922478e4ffe307c7309c17feb0888eb46b27f
KamiteAkira0611 <kamiteakira@gmail.com>
2022-02-12 20:30:17 +0900

    save

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  exportPathMap: async function (defaultPathMap) {
+    return {
+      ...defaultPathMap
+    }
+  }
 }
 
 module.exports = nextConfig
