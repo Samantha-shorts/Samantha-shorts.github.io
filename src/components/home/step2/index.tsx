360e4ee2d3b7ab6ef2252ecbe4671269fca0afcf
KamiteAkira0611 <kamiteakira@gmail.com>
2022-02-14 03:26:57 +0900

    save

--- src/components/home/step2/index.tsx
+++ src/components/home/step2/index.tsx
@@ -15,7 +15,7 @@ const Step2:NextPage = () => {
             centeredSlides={true}
             loop={true}
             autoplay={{
-              delay: 5000,
+              delay: 3000,
               disableOnInteraction: false,
             }}
             className="videoSwiper"
@@ -23,27 +23,33 @@ const Step2:NextPage = () => {
           >
             <SwiperSlide>
               <p>＼グッとくるならこれ／</p>
-              <img src="/images/video3.jpeg" alt="" />
+              <img className="pc" src="/images/video3.jpeg" alt="" />
+              <img className="sp" src="/images/video4.jpeg" alt="" />
             </SwiperSlide>
             <SwiperSlide>
               <p>＼本物の愛を💖／</p>
-              <img src="/images/video2.jpeg" alt="" />
+              <img className="pc" src="/images/video2.jpeg" alt="" />
+              <img className="sp" src="/images/video5.jpeg" alt="" />
             </SwiperSlide>
             <SwiperSlide>
               <p>＼スカッとできる✨／</p>
-              <img src="/images/video1.jpeg" alt="" />
+              <img className="pc" src="/images/video1.jpeg" alt="" />
+              <img className="sp" src="/images/video6.jpeg" alt="" />
             </SwiperSlide>
             <SwiperSlide>
               <p>＼グッとくるならこれ／</p>
-              <img src="/images/video3.jpeg" alt="" />
+              <img className="pc" src="/images/video3.jpeg" alt="" />
+              <img className="sp" src="/images/video4.jpeg" alt="" />
             </SwiperSlide>
             <SwiperSlide>
               <p>＼本物の愛を💖／</p>
-              <img src="/images/video2.jpeg" alt="" />
+              <img className="pc" src="/images/video2.jpeg" alt="" />
+              <img className="sp" src="/images/video5.jpeg" alt="" />
             </SwiperSlide>
             <SwiperSlide>
               <p>＼スカッとできる✨／</p>
-              <img src="/images/video1.jpeg" alt="" />
+              <img className="pc" src="/images/video1.jpeg" alt="" />
+              <img className="sp" src="/images/video6.jpeg" alt="" />
             </SwiperSlide>
           </Swiper>
         </div>
