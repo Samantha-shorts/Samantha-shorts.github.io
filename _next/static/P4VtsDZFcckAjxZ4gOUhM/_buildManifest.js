d18895b56615d15a3d853c045684fecd57c9f22e
Herosdb <Herosdb@users.noreply.github.com>
2022-03-09 02:23:26 +0000

    deploy: 46ae06e19d5c8204caf67f07f268a9e949e1c926

--- _next/static/P4VtsDZFcckAjxZ4gOUhM/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/487-15bb19adc6738a65.js","static/css/ce6747f1c51eea4d.css","static/chunks/pages/index-6a3d41f1d6020ca2.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
