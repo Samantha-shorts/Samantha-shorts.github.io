e9c0a8f9400acd4a058a2c4f1e63d1d2f935352d
Herosdb <Herosdb@users.noreply.github.com>
2022-03-18 05:13:58 +0000

    deploy: 24e8feb8f87e4a357f3fceb1e6839d0e0b775492

--- _next/static/cGBeZJFb8ebL75YNNFOG7/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/107-06ae5a3b14e9aa82.js","static/css/0a993e87026c6884.css","static/chunks/pages/index-039309f41faea059.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
